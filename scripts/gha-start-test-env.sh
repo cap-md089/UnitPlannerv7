4d86a76512b2c38b2e5d9687ecffd7c118e18c06
Andrew Rioux <rioux.d.andrew@gmail.com>
2022-04-05 12:26:28 -0400

    maybe resolving 404 with reactor tests

--- scripts/gha-start-test-env.sh
+++ scripts/gha-start-test-env.sh
@@ -21,4 +21,4 @@ skaffold run -p test,-dev
 
 kubectl get -n unitplannerv7 ingress
 kubectl wait -n ingress-nginx deployment --for condition=Available=True -l app.kubernetes.io/name=ingress-nginx
-sudo -E kubectl port-forward -n ingress-nginx service/ingress-nginx-controller 80:80 &
+kubectl wait -n unitplannerv7 deployment --for condition=Available=True -l name=client-reactor
